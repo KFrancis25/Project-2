0a3307be12065644c5e6222136aff338a466c644
Drew Guarnera <ryonek25@wooster.edu>
2024-10-23 12:53:40 -0400

    Fixed emergency fund and added to savings goal

--- Budget-Tool.py
+++ Budget-Tool.py
@@ -6,11 +6,14 @@ class BudgetApp:
         self.expenses = {}
         self.savings_goal = 0
         self.bank_balance = 0
-
+        self.account = 0
+        
+    # Ryan Yonek
     # Adds income to total income
     def add_income(self):
         self.income = self.income + float(input("Enter income amount to be added: $"))
 
+    # Ryan Yonek
     # Removes income from total income
     def remove_income(self):
         remove_income = float(input("Enter income amount to be removed: $"))
@@ -53,20 +56,11 @@ class BudgetApp:
     
     def calculate_50_30_20_rule(self):
         #Calculate the 50-30-20 rule distribution of the income.
-        print("Total income is: $", self.income)
+        self.add_income()
         needs = 0.5 * self.income
         wants = 0.3 * self.income
         savings = 0.2 * self.income
         return needs, wants, savings
-    
-    # def set_savings_goal(self):
-    #     #Set a financial goal for saving."""
-    #     try:
-    #         # User enters their savings goal
-    #         self.savings_goal = float(input("Enter your savings goal: $"))
-    #         print(f"Savings goal set to: ${self.set_savings_goal():.2f}")
-    #     except ValueError:
-    #         print("Invalid input. Please enter a numeric value.")
     
     def track_progress(self):
         saved_amount = self.bank_balance - sum(self.expenses.values())
@@ -80,17 +74,28 @@ class BudgetApp:
             return {category: amount for category, amount in self.expenses.items()}
         elif timeframe == "weekly":
             return {category: amount/ 4 for category, amount in self.expenses.items()}     
+
+    #tentative 50-30-20 rule (for testing)
+    def bank_account_based_budgeting(self):
+        needs, wants, savings = self.calculate_50_30_20_rule()
+        needs_allocated = self.bank_balance * 0.5
+        wants_allocated = self.bank_balance * 0.3
+        savings_allocated = self.bank_balance * 0.2
+        return needs_allocated, wants_allocated, savings_allocated
     
     #emergency fund creation
-    # Need to make a method that takes money from the uses
+    # Keith Young, Ryan Yonek
     def emergency_fund(self, deposit):
-        account = 0
-        if deposit < (self.income * .10):
-            print(f"You are adding ${deposit} to emergency fund and removing from your overall income.")
-            self.income = self.income - deposit
-            account += deposit
+        if deposit < (self.get_income() * .10):
+            print(f"You want add {deposit} to your emergency fund.")
+            self.account += deposit
         else:
             print("You are putting too much in your emergency fund!!!")
+    
+    # Returns emergency fund amount
+    # Ryan Yonek
+    def get_emergency_fund(self):
+        return self.account
 
 
 
@@ -102,7 +107,9 @@ class Goals:
         self.goal = goal
         self.cost = cost
         self.weeks = weeks
-        print(f"My goal is to buy a {self.goal} in {self.weeks} weeks. It cost ${self.cost}")
+    
+    def check_goal(self):
+        return self.goal, self.cost, self.weeks
         
     #Payment towards the goal
     def payment(self, money):
@@ -111,7 +118,7 @@ class Goals:
             total_left = cost - money
             print(total_left)
         except:
-            money < BudgetApp.get_income * 0.3
+            money < self.get_income() * 0.3
             print("You don't have enough money right now")
         
 
@@ -123,7 +130,6 @@ def main():
     while True:
         #feature selction
         print("\nChoose a feature:")
-        print(f"Current income is: ${app.income:.2f}")
         print("1. Financial Goals")
         print("2. Projected Amount")
         print("3. Monthly/Weekly Expenses")
@@ -148,18 +154,31 @@ def main():
             set_goal = input("Do you want to create a personal goal?: Enter y/n ")
             # Ask the user if they want to set personal purchase goal. If answer is yes it will record their goal.
             try: 
-                print(set_goal)
+                if set_goal == 'y':
+                    ask_goal = input("What are you saving for? ")
+                    ask_cost = int(input("What is the cost? $")) 
+                    ask_weeks = int(input("In how many weeks do you want to accomplish this goal? "))
+                    goals = Goals(ask_goal, ask_cost, ask_weeks)
+                    print(f"My goal is to buy a {goals.goal} in {goals.weeks} weeks. It cost ${goals.cost}")
+                    add_to_goal = input("Do you want to make a payment to the personal goal?: Enter y/n ")
+                    try:
+                        if add_to_goal == 'y':
+                            money = input("How much would you like to pay towards the goal? $")
+                            goals.payment(money)
+                            print("Current goal is now: ", goals.check_goal())
+                        if add_to_goal == 'n':
+                            print("Back to main menu")
+                    except:
+                        if set_goal != "y" or "n":
+                            print("Enter a y or n for yes or no!")
+                if set_goal == 'n':
+                    print("No savings goal set. Back to main menu")
             except:
                 if set_goal != "y" or "n":
                     print ("Enter a y or n for yes or no!")
-            if set_goal == "y":
-                ask_goal = input("What is your goal? ")
-                ask_cost = int(input("What is the cost? ")) 
-                ask_weeks = int(input("In how many weeks do you want to accomplish this goal?"))
-                personal_goal = Goals(ask_goal, ask_cost, ask_weeks)
-                print(personal_goal)
             else:
-                print("No personal goal!")
+                print("No personal goal! Did not enter 'y' or 'n'. Back to main menu.")
+
     
 
         elif choice == 2:
@@ -188,13 +207,12 @@ def main():
             print("\n--- Income Adjustment ---")
             print("1. Add to total income")
             print("2. Remove from total income")
-            print("3. Check total income")
-            print("4. Reset income to 0")
-            print("5. Back to main menu.")
+            print("3. Reset income to 0")
+            print("4. Back to main menu.")
             try:
-                adjchoice = int(input("Enter your choice (1-5): "))
+                adjchoice = int(input("Enter your choice (1-4): "))
             except ValueError:
-                print("Please enter a valid choice (1-5).")
+                print("Please enter a valid choice (1-4).")
                 continue
             if adjchoice == 1:
                 app.add_income()
@@ -203,17 +221,25 @@ def main():
                 app.remove_income()
                 print(f"New total income is: ${app.income:.2f}")
             if adjchoice == 3:
-                print(f"Total income is: ${app.income:.2f}")
-            if adjchoice == 4:
                 app.income = 0
                 print(f"New total income is: ${app.income:.2f}")
-            if adjchoice == 5:
+            if adjchoice == 4:
                 print("Exiting back to main budget menu.")
                                
 
         elif choice == 6:
-            emergency_money = int(input("How much would you like to add to your emergency fund? $"))
+            emergency_money = int(input("How much would you like to add to your emergency fund? "))
             app.emergency_fund(emergency_money)
+            try:
+                check_fund = input("Do you want to see emergency fund? y or n ")
+                if check_fund == 'y':
+                    print("Emergency fund is: ", app.get_emergency_fund())
+                if check_fund == 'n':
+                    print("Going back to main menu")
+            except:
+                if check_fund != 'y' or 'n':
+                    print('Answer y or n for yes or no.')
+            
 
         elif choice == 7:
             #exit the program
